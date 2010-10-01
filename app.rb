13906c3fa3eadc335ae3844fbf0bb5249ee7019d
David Pick <pickdavid@gmail.com>
2010-10-01 16:42:29 -0400

    finished

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'sinatra'
+require 'itunes_mac'
+
+configure do
+  $itunes = Itunes.new
+end
+
+get '/' do
+  if $itunes.launched?
+    @title = $itunes.current_track
+    @artist = $itunes.current_artist
+    @album = $itunes.current_album
+  end
+
+  haml :index
+end
+
+post '/musicplayer' do
+  params.each { |k, v| $itunes.send(k) if $itunes.respond_to?(k) }
+  redirect '/'
+end
---
+++ itunes_mac.rb
@@ -0,0 +1,50 @@
+class Itunes
+  def playpause
+    tell_to 'playpause'
+  end
+
+  def prev
+    tell_to 'previous track'
+  end
+
+  def next
+    tell_to 'next track'
+  end
+
+  def voldown
+    tell_to 'set sound volume to sound volume - 10'
+  end
+
+  def volup
+    tell_to 'set sound volume to sound volume + 10'
+  end
+
+  def volume
+    tell_to 'return sound volume'
+  end
+
+  def current_track
+    tell_to 'return name of current track'
+  end
+
+  def current_artist
+    tell_to 'return (artist of current track)'
+  end
+
+  def current_album
+    tell_to 'return (album of current track)'
+  end
+
+  def launched?
+    %x(osascript -e 'tell app "System Events" to count (every process whose name is "#{name}")')
+  end
+
+  def name
+    "iTunes"
+  end
+
+  private
+  def tell_to(command)
+    %x(osascript -e 'tell app "#{name}" to #{command}')
+  end
+end
