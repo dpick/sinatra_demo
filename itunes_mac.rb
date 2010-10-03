7554f981af43b43c6d488ab1a2dea6b0c07d3e42
David Pick <pickdavid@gmail.com>
2010-10-03 00:41:46 -0400

    refactoring the itunes class

--- itunes_mac.rb
+++ itunes_mac.rb
@@ -1,30 +1,30 @@
 class Itunes
   def playpause
-    do_command 'playpause'
+    run_command 'playpause'
   end
 
   def prev
-    do_command 'previous track'
+    run_command 'previous track'
   end
 
   def next
-    do_command 'next track'
+    run_command 'next track'
   end
 
   def voldown
-    do_command 'set sound volume to sound volume - 10'
+    run_command 'set sound volume to sound volume - 10'
   end
 
   def volup
-    do_command 'set sound volume to sound volume + 10'
+    run_command 'set sound volume to sound volume + 10'
   end
 
   def volume
-    do_command 'return sound volume'
+    run_command 'return sound volume'
   end
 
   def current(attribute)
-    do_command "return #{attribute} of current track"
+    run_command "return #{attribute} of current track"
   end
 
   def launched?
@@ -32,7 +32,7 @@ class Itunes
   end
 
   private
-  def do_command(command)
+  def run_command(command)
     %x(osascript -e 'tell app "itunes" to #{command}')
   end
 end
